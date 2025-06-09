cccb21b9be8a317f761cea3f9e239d744a61c539
Jasper Van Zeir <49187306+R3D-R4V3N@users.noreply.github.com>
2025-06-09 14:59:17 +0200

    Shift overtake stats to past races and drop from feature lists

--- infer.py
+++ infer.py
@@ -35,8 +35,7 @@ def inference_for_date(cutoff_date):
         'grid_position', 'Q1_sec', 'Q2_sec', 'Q3_sec',
         'month', 'weekday', 'avg_finish_pos', 'avg_grid_pos', 'avg_const_finish',
         'air_temperature', 'track_temperature', 'grid_diff', 'Q3_diff', 'grid_temp_int',
-        'circuit_country', 'circuit_city',
-        'overtakes_count', 'weighted_overtakes'
+        'circuit_country', 'circuit_city'
     ]
     X_test = df_test[feature_cols]
 
